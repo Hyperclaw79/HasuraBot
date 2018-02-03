bc7963f0ba3c61fa14563dcc7e0a82f667e24639
Harshith Thota <harshith.thota7@gmail.com>
2018-02-03 21:56:31 +0530

    HasuraBot

---
+++ README.md
@@ -0,0 +1,4 @@
+# HasuraBot
+
+Readme coming soon.....
+For now, just open a PR if you added/edited any commands.
---
+++ microservices/discordbot/src/bot.py
@@ -0,0 +1,371 @@
+import discord
+import inspect
+import aiohttp
+import os
+import asyncio
+from textwrap import dedent
+
+discord_token = os.environ["DISCORD_TOKEN"]
+
+class Response:
+    def __init__(self, content, reply=False, delete_after=0):
+        self.content = content
+        self.reply = reply
+        self.delete_after = delete_after
+
+class HasuraBot(discord.Client):
+    def __init__(self):
+        self.prefix = '*'
+        super().__init__()
+        self.aiosession = aiohttp.ClientSession(loop=self.loop)
+        self.http.user_agent += ' HasuraBot/1.0'
+        
+
+    async def on_ready(self):
+        print('HasuraBot is now live!')
+        game = discord.Game(name="the byte crunching game. #HasuraFTW")
+        await self.change_presence(game=game)
+
+
+    async def cmd_ping(self, message):
+        """
+        Usage:
+            {command_prefix}ping
+                        
+        Is it alive??
+        """
+        await message.channel.send("Pong!")
+
+    async def cmd_say(self, message):
+        """
+        Usage:
+            {command_prefix}say [your message]
+                        
+        Echoes your message after deleting it.
+        """
+        msg = message.content.replace("{}say".format(self.prefix),'').strip() 
+        await message.channel.send(msg)
+        try:
+            await message.delete()    
+        except:
+            print('Jeez! I need better permissions in {}.'.format(message.guild))
+
+    async def cmd_shrug(self, message):
+        """
+        Usage:
+            {command_prefix}shrug
+                        
+        ASCII Shrugimation.
+        """
+        await message.delete()
+        shrugList = ["`¯\__(ツ)/¯`", "`¯\_(ツ)_/¯`", "`¯\(ツ)__/¯`", "`¯\_(ツ)_/¯`"]
+        lulz = await message.channel.send(shrugList[1])
+        i = 2
+        while i < 400:
+            await lulz.edit(content=shrugList[i%4])
+            i = i + 1    
+
+    async def cmd_wow(self, message):
+        """
+        Usage:
+            {command_prefix}wow [your message]
+                        
+        Stylize your message using emoji letters.
+        """
+        message_content = message.content.strip()
+        chan = message.channel
+        await message.delete()
+        mesg = message_content.replace('{}wow'.format(self.prefix), '')
+
+        t = ""
+        nums = ['zero','one','two','three','four','five','six','seven','eight','nine']
+        for c in mesg:
+           if ord(c)<90 and ord(c)>=65:
+              c = chr(ord(c)+32)
+              c = ":regional_indicator_{}: ".format(c)
+           elif c == " ":
+              c = "  "
+           elif ord(c)>=97 and ord(c)<=122:
+              c = ":regional_indicator_{}: ".format(c)
+           elif int(c)>=0 and int(c)<=9: 
+              c = ":{}:".format(nums[int(c)])
+           t = t + c
+        await chan.send('\n{}'.format(t))            
+
+    async def cmd_ppap(self, message):
+       """
+       Usage: 
+       {command_prefix}ppap
+
+       PPAP cancer, bot version.
+       """
+       mes = await message.channel.send("`¯\_(ツ)_/¯`")
+       await mes.edit(content=":pen_ballpoint:         \n¯\_(ツ)_/¯")
+       await asyncio.sleep(1)	   
+       await mes.edit(content=":pen_ballpoint:         :apple:\n¯\_(ツ)_/¯")
+       await asyncio.sleep(1) 	   
+       await mes.edit(content=":apple::pen_ballpoint:")
+       await asyncio.sleep(2.5)
+       await mes.edit(content=":pen_ballpoint:         \n¯\_(ツ)_/¯")
+       await asyncio.sleep(1) 	   
+       await mes.edit(content=":pen_ballpoint:         :pineapple:\n¯\_(ツ)_/¯")
+       await asyncio.sleep(1)	   
+       await mes.edit(content=":pineapple::pen_ballpoint:")
+       await asyncio.sleep(2.5)	   
+       await mes.edit(content=":apple::pen_ballpoint:")
+       await asyncio.sleep(1)	   
+       await mes.edit(content=":pineapple::pen_ballpoint:")
+       await asyncio.sleep(2.5) 	   
+       await mes.edit(content=":pen_ballpoint:")		
+       await mes.edit(content=":pen_ballpoint::pineapple:")		
+       await mes.edit(content=":pen_ballpoint::pineapple::apple:")	   
+       await mes.edit(content=":pen_ballpoint::pineapple::apple::pen_ballpoint:")    
+
+    async def cmd_moonwalk(self, message):
+       """
+       Usage:
+           {command_prefix}moonwalk
+                
+       Use to see an emoji perform moonwalk.
+       """
+       m1 = await message.channel.send(".:walking:")	
+       l = [":runner:",":walking:"]
+       t = "."
+       for i in range(25):
+          t = t + "."
+          s = t+l[i%2]
+          await m1.edit(content=s)
+
+    async def cmd_react(self, message):
+        """
+        Usage:
+            {command_prefix}react [reaction1 reaction2 ....]
+                        
+        Add a list of reactions to the previous message. Separate the emojis with spaces.
+        """
+        target = await message.channel.history(limit=1, before=message).next()
+        await message.delete()
+        reactions = message.content.replace('{}react ','').split(' ')
+        for reaction in reactions:
+            try:
+                await target.add_reaction(reaction)
+            except:
+                pass    
+    
+    async def cmd_iam(self, message):
+        """
+        Usage:
+            {command_prefix}iam role
+                        
+        Assign yourself the entry level role.
+        """
+        def check1(reaction, clicker):
+            return clicker == user and reaction.message.id == base.id and reaction.emoji in list(roles_dict.keys())
+        intent = message.content.split('{}iam '.format(self.prefix))[1].split(' ')[0].strip()
+        user = message.author
+        guild = self.get_guild(int(os.environ["GUILD_ID"]))
+        user = guild.get_member(user.id)             
+        if intent.lower() == "intern":
+            intern = [role for role in guild.roles if role.name == "hpdf-intern"][0]
+            react = [role for role in guild.roles if role.name == "hpdf-reactjs"][0]
+            native = [role for role in guild.roles if role.name == "hpdf-react-native"][0]
+            express = [role for role in guild.roles if role.name == "hpdf-nodejs-express"][0]
+            flask = [role for role in guild.roles if role.name == "hpdf-python-flask"][0]
+            react_icon = [emoji for emoji in guild.emojis if emoji.name == "reactjs"][0]
+            native_icon = [emoji for emoji in guild.emojis if emoji.name == "reactnative"][0]        
+            express_icon = [emoji for emoji in guild.emojis if emoji.name == "node"][0]
+            flask_icon = [emoji for emoji in guild.emojis if emoji.name == "flask"][0]
+            roles_dict = {
+                            react_icon: react,
+                            native_icon: native,
+                            express_icon: express,
+                            flask_icon: flask
+                        }
+            await user.add_roles(intern)
+            base = await user.send("Hey {}, you've successfully been assigned the `@hpdf-intern` role.".format(user.mention) + \
+                    "Please react to this message with your appropriate framework.")
+            for reaction in list(roles_dict.keys()):
+                await base.add_reaction(reaction)
+            reaction, clicker = await self.wait_for('reaction_add', check=check1)
+            role = roles_dict[reaction.emoji]
+            await user.add_roles(role)
+            await base.delete()
+            final = await user.send("{}, you've been successfully assigned the `@{}` role. :thumbsup:".format(user.mention, role))
+            await user.send("Please keep HPDF conversations in channel under the `HPDF` category.")
+            await asyncio.sleep(120)
+            await final.delete()
+            log = guild.get_channel(int(os.environ["LOG_CHANNEL"]))
+            await log.send('User {} has been assigned `@{}` role and they chose the framework `@{}`.'.format(user.name+'#'+user.discriminator, intern, role))
+        elif intent.lower() == "ca":
+            ca = [role for role in guild.roles if role.name == "HasuraCA"][0]
+            await user.add_roles(ca)
+            await user.send("Successfully assigned you as `@{}`. :thumbsup:".format(ca))
+            await user.send("Please keep the ca related conversation in #hasura-campus-ambassadors." + \
+                            "\nFeel free to chat about general things in the public channel.")
+            log = guild.get_channel(int(os.environ["LOG_CHANNEL"]))
+            await log.send('User {} has been assigned `@{}` role.'.format(user.name+'#'+user.discriminator, role))
+        else:
+            await user.send('You must either choose `intern` or `CA`.')    
+
+    async def cmd_iamnot(self, message):
+        """
+        Usage:
+            {command_prefix}iamnot [your message]
+                        
+        Remove a role from yourself.
+        """
+        param = message.content.split('{}iamnot '.format(self.prefix))[1].split(' ')[0].strip()
+        guild = self.get_guild(int(os.environ["GUILD_ID"]))
+        role = [role for role in guild.roles if role.name.lower() == param.lower()][0]  
+        user = message.author
+        user = guild.get_member(user.id)       
+        await user.remove_roles(role)
+        await user.send("Successfully removed the `@{}` role. :thumbsup:".format(role))
+        log = guild.get_channel(int(os.environ["LOG_CHANNEL"]))
+        await log.send('User {} has removed the `@{}` role for themselves.'.format(user.name+'#'+user.discriminator, role))
+
+    async def cmd_prune(self, message):
+        """
+        Usage:
+            {command_prefix}prune
+                        
+        Deletes last X messages. Mods only.
+        """
+        role = str(message.author.top_role)
+        mods = ["admin","team hasura","moderator"]
+        if role in mods:
+            try:
+                count = int(message.content.split('{}prune '.format(self.prefix))[1].split(' ')[0].strip()) + 1
+                try:
+                    await message.channel.purge(limit=count)
+                    notif = await message.channel.send("Successfully deleted the last {} messages. :thumbsup:".format(count))
+                    await asyncio.sleep(30)
+                    await notif.delete()
+                except PermissionError:
+                    await message.channel.send("Looks like I don't have permission to delete messages here. :eyes:")        
+                except Exception as e:
+                    print(str(e))        
+            except IndexError:
+                notif = await message.channel.send("You didn't provide the count.")
+                await asyncio.sleep(30)
+                await notif.delete()
+        else:
+            await message.author.send("Sorry but only the mods can prune messages. :sweat_smile:")
+
+    async def cmd_help(self, message):
+        """
+        Usage:
+            {command_prefix}help [command]
+        
+        Prints a help message.
+        If a command is specified, it prints a help message for that command.
+        Otherwise, it lists the available commands.
+        """
+        try:
+            command = message.content.split("{}help ".format(self.prefix))[1].strip()
+            cmdc = getattr(self, 'cmd_' + command, None)
+            if cmdc:
+                content = dedent(cmdc.__doc__).replace('{command_prefix}', '@   ' + self.prefix)
+                content = content.replace('Usage','Usage (exclude the @)').replace('\n\n','\n')
+                await message.author.send('```py\n{}```'.format(content))
+            else:
+                await message.author.send('No such command')
+        except:
+            msg1 = await message.author.send('**HasuraBot Commands List:**\n')
+            commands = []
+            cmdc = {}
+            #txt2 = ''
+            #txt3 = ''
+            for att in dir(self):
+                if att.startswith('cmd_') and att != 'cmd_help':
+                    try:
+                        atc = getattr(self, att)
+                        print(atc.__doc__ + '\n')
+                        cmdc[att] = dedent(atc.__doc__.split('\n')[4])
+                    except:
+                        print('No docstring written for: ' + att)
+            ''' 
+            msg5 = await self.send_message(message.channel, '__Total number of commands__: **%d**' % comlen)
+            count = 0
+            for att in cmdc:
+                count += 1
+                if count < 15:
+                    txt1 += dedent('```md\n<%s>```' % att.replace('cmd_', self.config.command_prefix) + '```diff\n-%s```\n' % cmdc[att])
+                elif count > 15 and count < 30:
+                    txt2 += dedent('```md\n<%s>```' % att.replace('cmd_', self.config.command_prefix) + '```diff\n-%s```\n' % cmdc[att])
+                else:
+                    txt3 += dedent('```md\n<%s>```' % att.replace('cmd_', self.config.command_prefix) + '```diff\n-%s```\n' % cmdc[att])
+
+            msg2 = await (self.send_message(message.channel, txt1))
+            msg3 = await (self.send_message(message.channel, txt2))
+            msg4 = await (self.send_message(message.channel, txt3))
+            await asyncio.sleep(300)
+            await self.delete_message(msg1)
+            await self.delete_message(msg5)
+            await self.delete_message(msg2)
+            await self.delete_message(msg3)
+            await self.delete_message(msg4) '''
+            comlen = len(cmdc)
+            delme = await message.author.send('__Total number of commands__: **{}**\n'.format(comlen))
+            txt1 = "```md\n"
+            for att in cmdc:
+                txt1 += dedent('[{}]( {})\n' .format(att.replace('cmd_', self.prefix),cmdc[att]))
+            txt1 += "```"    
+            await delme.edit(content=delme.content+txt1)
+            await asyncio.sleep(300)
+            await msg1.delete()
+            await delme.delete()
+
+    async def on_message(self, message):
+        if self.prefix not in message.content:
+            return
+        
+        # we do not want the bot to reply to itself or other bots
+        if message.author.id == self.user.id or message.author.bot:
+            return
+
+        if message.content == "(╯°□°）╯︵ ┻━┻":
+            await message.channel.send("┬─┬ ノ( ゜-゜ノ)")    
+
+        #----------------------------------------------------------------------------#
+        # Don't worry about this part. We are just defining **kwargs for later use.
+        cmd, *args = message.content.split(' ') # The first word is cmd, everything else is args. 
+        cmd = cmd[len(self.prefix):].lower().strip() # For '$', cmd = cmd[1:0]. Eg. $help -> cmd = help
+        handler = getattr(self,'cmd_{}'.format(cmd),None) # Checks if MyBot has an attribute called cmd_command (cmd_help).
+        if not handler: # The command given doesn't exist in our code, so ignore it.
+            return
+        prms = inspect.signature(handler) # If attr is defined as async def help(a,b='test',c), prms = (a,b='test',c)
+        params = prms.parameters.copy() # Copy since parameters are immutable.
+        h_kwargs = {}                   # Dict for group testing all the attrs.
+        if params.pop('message',None):
+            h_kwargs['message'] = message
+        if params.pop('channel',None):
+            h_kwargs['channel'] = message.channel
+        if params.pop('guild',None):
+            h_kwargs['guild'] = message.guild
+        if params.pop('mentions',None):
+            h_kwargs['mentions'] = list(map(message.server.get_member, message.raw_mentions)) # Gets the user for the raw mention and repeats for every user in the guild.            
+        if params.pop('args',None):
+            h_kwargs['args'] = args
+
+        # For remaining undefined keywords:
+        for key, param in list(params.items()):
+            if not args and param.default is not inspect.Parameter.empty: # Junk parameter present for attribute 
+                params.pop(key) 
+                continue        # We don't want that in our tester.
+
+            if args:
+                h_kwargs[key] = args.pop(0) # Binding keys to respective args.
+                params.pop(key)
+
+        # Time to call the test.
+        res = await handler(**h_kwargs)
+        if res and isinstance(res, Response): # Valid Response object
+                content = res.content
+                if res.reply:
+                    content = '{},{}'.format(message.author.mention, content)
+
+                sentmsg = await message.channel.send(content)
+
+bot = HasuraBot()    
+bot.run(discord_token)
