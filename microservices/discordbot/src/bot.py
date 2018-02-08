13ca1c9ac26b0db90248f9fa87ad356f454bc7a2
Harshith Thota <harshith.thota7@gmail.com>
2018-02-08 14:55:03 +0530

    Hub command algolia debug.

--- microservices/discordbot/src/bot.py
+++ microservices/discordbot/src/bot.py
@@ -407,8 +407,8 @@ class HasuraBot(discord.Client):
                     await base.add_reaction(reaction)
                 reaction, user = await self.wait_for('reaction_add',check=check)
                 choice = reaction_list.index(reaction.emoji)
-                print(param_list[choice])
                 projects = self.hubber.query(param=param_list[choice])
+                print(projects)
                 await base.clear_reactions()
             else:
                 base = await message.channel.send("{} Fetching results. Please wait. :hourglass_flowing_sand:".format(message.author.mention))
