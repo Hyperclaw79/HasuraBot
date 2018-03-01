d764ca646fab370b7a2dc2538153d3ba2d58d01f
Harshith Thota <harshith.thota7@gmail.com>
2018-03-02 00:00:35 +0530

    Modifications to brain + updated secrets.

--- microservices/discordbot/src/utils/brain.py
+++ microservices/discordbot/src/utils/brain.py
@@ -1,6 +1,7 @@
 import json
 import aiohttp
 import logging
+import random
 
 logging.getLogger('asyncio').setLevel(logging.CRITICAL)
 
@@ -9,29 +10,37 @@ class Brain:
         self.user = user
         self.key = key
         self.nick = nick
-        body = {
-            'user': user,
-            'key': key,
-            'nick': nick
-        }
+        self.body = {
+                    'user': self.user,
+                    'key': self.key,
+                    'nick': self.nick
+                }
         self.loop = loop
         self.sess = aiohttp.ClientSession(loop=self.loop)
-        self.sess.post('https://cleverbot.io/1.0/create', json=body)
+        print("Initiated Brain ClientSession.")
+
+    async def create(self):
+        with aiohttp.Timeout(10):
+            async with self.sess.post('https://cleverbot.io/1.0/create', json=self.body) as resp:
+                r = await resp.json()
+        if resp.status == 200:
+            if r["status"] is not "success":
+                self.nick += '-{}'.format(random.randint(1,1000))
+                self.create()
+            return "API is online. Using clever mode."
+        else:
+            return "API is down. Will notify the users."
+            
         
     async def query(self, text):
-        body = {
-            'user': self.user,
-            'key': self.key,
-            'nick': self.nick,
-            'text': text
-        }
+        self.body['text'] = text
         try:
             with aiohttp.Timeout(10):
-                async with self.sess.post('https://cleverbot.io/1.0/ask', json=body) as resp:
+                async with self.sess.post('https://cleverbot.io/1.0/ask', json=self.body) as resp:
                     r = await resp.json()
 
             if r['status'] == 'success':
                 return r['response'], 200
         except:
-            return "Looks like the CleverBot.IO api is down. Please try later.", 500
+            return "Looks like the CleverBot.IO api is down. Please try again later.", 500
 
